460b1172f46e81a783f2478a7a3526c366333e1d
BertrandB <git.20.BertrandB@spamgourmet.com>
2014-02-24 18:22:58 +0100

    Revert "Wordpress oubli de Wp_Lang"
    This reverts commit 90c874c32f1ad6f3839e89c59e5b9919db685245.

--- .openshift/config/wp-config.php
+++ .openshift/config/wp-config.php
@@ -107,7 +107,7 @@ $table_prefix  = 'wp_';
  * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
  * language support.
  */
-define('WPLANG', 'fr_FR');
+define('WPLANG', '');
 
 /**
  * For developers: WordPress debugging mode.
